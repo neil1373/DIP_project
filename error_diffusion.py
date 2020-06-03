15b8b8206eb6486e207e2eeb9f29b3c4277e1831
Neil1373 <b06902002@ntu.edu.tw>
2020-06-03 21:39:29 +0800

    Update README

--- error_diffusion.py
+++ error_diffusion.py
@@ -1,19 +1,10 @@
 import numpy as np
 from numba import jit
-<<<<<<< HEAD
-
-@jit # by Stanford - 死電肥爾
-
-def error_diffusion(img):
-	img_height = img.shape[0]
-	img_width = img.shape[1]
-=======
 # from distributionOfSIP import SIPassignment
 
 @jit
 def error_diffusion(img):
 	img_height, img_width = img.shape
->>>>>>> 7d8c1151d69f0cbebfe4ae61ac6fc06e90c49319
 	thres = 0.5
 	F = img / 255
 	total_error = np.zeros((img_height, img_width))
@@ -27,15 +18,6 @@ def error_diffusion(img):
 				error[i,j] = F[i,j] - 1
 			else:
 				error[i,j] = F[i,j]
-<<<<<<< HEAD
-	for i in range(img_height):
-		for j in range(img_width):
-			for m in range(3):
-				for n in range(3):
-					if ((i + m - 1) >= 0 and (i + m - 1) < img_height and (j + n - 1) >= 0 and (j + n - 1) < img_width):
-						total_error[i+m-1, j+n-1] += error[i,j] * floyd_mask[m,n]
-	F += total_error
-=======
 			for m in range(3):
 				for n in range(3):
 					try:
@@ -43,7 +25,6 @@ def error_diffusion(img):
 						F[i+m-1, j+n-1] += error[i,j] * floyd_mask[m,n]
 					except:
 						pass
->>>>>>> 7d8c1151d69f0cbebfe4ae61ac6fc06e90c49319
 
 	G = np.zeros((img_height, img_width))
 	full = False
@@ -52,15 +33,9 @@ def error_diffusion(img):
 			break
 
 		for j in range(img_width):
-<<<<<<< HEAD
-			if (SIPassignment()):
-				full = True
-				break
-=======
 			# if (SIPassignment(i, j)):
 			# 	full = True
 			# 	break
->>>>>>> 7d8c1151d69f0cbebfe4ae61ac6fc06e90c49319
 
 			if (F[i,j] >= 0.5):
 				G[i][j] = 255
