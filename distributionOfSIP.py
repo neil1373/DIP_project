b6abddb14a4653b9a85a4f36ea600279387a5a0f
b06902115 <b06902115@ntu.edu.tw>
2020-06-05 16:12:21 +0800

    distributionOfSIP

---
+++ distributionOfSIP.py
@@ -0,0 +1,12 @@
+def SIPassignment(img, share, Z0):
+	
+	c0 = np.array([[0,0,1,1],[0,1,0,1],[0,1,1,0]])
+	c1 = np.array([[1,1,0,0],[1,0,1,0],[1,0,0,1]])
+	
+	img_height, img_width = img.shape
+
+	for i in range(img_height):
+		for j in range(img_width):
+
+
+	return 0
